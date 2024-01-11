4f6e999deb176d402e4b2c42b963a79b884c576c
Minpick <152726605+Minpick@users.noreply.github.com>
2024-01-11 23:07:35 +0400

    Perenos

--- src/LandingPage/Sections/HeaderSection/HeaderSection.jsx
+++ src/LandingPage/Sections/HeaderSection/HeaderSection.jsx
@@ -1,38 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './HeaderSection.module.scss'
 import logosite from '../../images/logosite.png'
+import classNames from 'classnames';
 
 const HeaderSection = () => {
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+   const [isBurgerFollow, setIsBurgerFollow] = useState(false);
+
+   const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+   };
+
+   const closeMenu = () => {
+      setIsMenuOpen(false);
+   };
+
+   const handleScroll = () => {
+      if (window.scrollY > 0) {
+         setIsBurgerFollow(true);
+      } else {
+         setIsBurgerFollow(false);
+      }
+   };
+
+   useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+         window.removeEventListener('scroll', handleScroll);
+      };
+   }, []);
+
+
+
    return (
-      <header className={style.header}>
-         <a href="#" className={style.burger}><span></span></a>
-         <div className={style.container}>
-            <div className={style.header__inner}>
-               <a className={style.header__logo} href="#">
-                  <img src={logosite} alt="Logo"/>
-               </a>
-               <nav className={style.header__menu}>
-                  <ul className={style.header__menu_list}>
-                     <li className={style.header__menu_item}>
-                        <a className={style.header__menu_link} href="#">Главная</a>
-                     </li>
-                     <li className={style.header__menu_item}>
-                        <a className={style.header__menu_link} href="#services">Услуги</a>
-                     </li>
-                     <li className={style.header__menu_item}>
-                        <a className={style.header__menu_link} href="#team">Наша команда</a>
-                     </li>
-                     <li className={style.header__menu_item}>
-                        <a className={style.header__menu_link} href="#footer">Наши контакты</a>
-                     </li>
-                     <li className={style.header__menu_item}>
-                        <a className={style.header__menu_link} href="auth.html">Личный кабинет</a>
-                     </li>
-                  </ul>
-               </nav>
+      <>
+         <div
+            className={classNames(style.overlay, isMenuOpen ? style.overlay_show : '')}
+            onClick={toggleMenu}></div>
+         <header className={style.header}>
+            <a href="#"
+               className={classNames(style.burger, isBurgerFollow ? style.burger_follow : '')} onClick={toggleMenu}><span></span></a>
+            <div className={style.container}>
+               <div className={classNames(style.header__inner, isMenuOpen ? style.header__inner_open : '')}>
+
+                  <a className={style.header__logo} href="#">
+                     <img src={logosite} alt="Logo" />
+                  </a>
+                  <nav className={style.header__menu}>
+                     <ul className={style.header__menu_list}>
+                        <li className={style.header__menu_item}>
+                           <a
+                              className={classNames(style.header__menu_link)}
+                              onClick={closeMenu}
+
+                              href="#">Главная</a>
+                        </li>
+                        <li className={style.header__menu_item}>
+                           <a
+                              className={classNames(style.header__menu_link)}
+                              onClick={closeMenu}
+                              href="#services">Услуги</a>
+                        </li>
+                        <li className={style.header__menu_item}>
+                           <a
+                              className={classNames(style.header__menu_link)}
+                              onClick={closeMenu}
+                              href="#team">Наша команда</a>
+                        </li>
+                        <li className={style.header__menu_item}>
+                           <a
+                              className={classNames(style.header__menu_link)}
+                              onClick={closeMenu}
+                              href="#footer">Наши контакты</a>
+                        </li>
+                        <li className={style.header__menu_item}>
+                           <a
+                              className={classNames(style.header__menu_link)}
+                              onClick={closeMenu}
+                              href="auth.html">Личный кабинет</a>
+                        </li>
+                     </ul>
+                  </nav>
+               </div>
             </div>
-         </div>
-      </header>
+         </header>
+
+      </>
    )
 }
 
