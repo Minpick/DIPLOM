bcd337bbd630db131cc5302715690a0d08df14cb
Minpick <152726605+Minpick@users.noreply.github.com>
2024-02-08 23:57:19 +0400

    Update DefaultPage.jsx

--- src/LaPage/Components/DefaultPage/DefaultPage.jsx
+++ src/LaPage/Components/DefaultPage/DefaultPage.jsx
@@ -4,12 +4,15 @@ import { NavLink, Outlet, useLocation, useSearchParams } from 'react-router-dom'
 import ParamsBtns from '../../UI/ParamsBtns/ParamsBtns'
 
 const DefaultPage = ({ li, children, statuses,addLi }) => {
-   const [searchParams, setSearchParams] = useSearchParams()
-   const status = searchParams.get('status')
-   if (status === 'produce') {
-      li[1] = 'Исполнитель'
-   } else {
-      li[1] = addLi
+
+   const location = useLocation()
+   if(location.pathname ==="/la/tasks"){
+
+      if (location.search === "?status=produce") {
+         li[1] = 'Исполнитель'
+      } else {
+         li[1] = addLi
+      }
    }
    const menu = li.map((item) => {
       return (
